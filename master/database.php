268d631e2a17302caee09f0984950f7c295dd00a
Ari Pratama <arypratama5@gmail.com>
2019-04-28 00:09:18 +0700

    Add database Comment for installation

--- master/database.php
+++ master/database.php
@@ -3,11 +3,11 @@
  * using mysqli_connect for database connection
  */
 
-$dbHost = '127.0.0.1';
-$dbName = 'atlanta';
-$dbUser = 'atlanta';
-$dbPass = 'v3PZ2F6r9enJXzz';
-$dbPort = '3306';
+$dbHost = '127.0.0.1';       // Database Host
+$dbName = 'atlanta';         // Database Name
+$dbUser = 'atlanta';         // Database Username
+$dbPass = 'v3PZ2F6r9enJXzz'; // Database Password
+$dbPort = '3306';            // Database port
 
 $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
 
